6eb277fe56882383774f8ded37507140debac954
Matt Heslington <matt.heslington@icloud.com>
2021-08-01 20:47:03 +0800

    :bug: remove the tailwind breakpointoverlay

--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,11 @@
 import Top from 'components/top'
 import Footer from 'components/footer'
 import Meta from 'components/meta'
-import ResponsiveGuide from 'components/ResponsiveGuide'
 
 export default function Layout({ children }) {
     return (
         <>
         <Meta />
-        <ResponsiveGuide/>
         <div className="min-h-screen bg-white dark:bg-black">
             <Top/>
             <main className="bg-white dark:bg-black">{children}</main>
